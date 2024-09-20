12994cf6a61650e226396c044b4fdc364b463e8b
Athish Thiruvengadam <athisht2@illinois.edu>
2024-09-20 15:45:51 -0500

    LVC Text conversion
    updated general parsing function to deal with empty strings
    
    LVC conversion deals with all notice types
    
    Added $schema field

--- gcn_classic_text_to_json/notices/sk_sn/__main__.py
+++
@@ -1,4 +0,0 @@
-from . import conversion as sk_sn_conversion
-
-if __name__ == "__main__":
-    sk_sn_conversion.create_all_sk_sn_jsons()
--- gcn_classic_text_to_json/notices/snews/__main__.py
+++
@@ -1,4 +0,0 @@
-from . import conversion
-
-if __name__ == "__main__":
-    conversion.create_all_snews_jsons()
